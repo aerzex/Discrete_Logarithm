3961faf5acdb99233b49b49cdb165752cceee53f
aerzex <aerzex@bk.ru>
2025-01-21 04:31:35 +1000

    Logic improvement
    Added algorithm to find all divisors of p - 1. This needs to find r - order of element a. More correct to solve congruence modulo r

--- src/rho_pollard.py
+++ src/rho_pollard.py
@@ -1,8 +1,8 @@
 import sys 
 import os
 import math
-lab1_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.', 'algorithms'))
-sys.path.append(lab1_path)
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.', 'algorithms'))
+sys.path.append(lib_path)
 from algorithms import algorithm_fast_pow, algorithm_comprasion, algorithm_rho_pollard_fact
 
 def algorithm_rho_pollard():
@@ -10,6 +10,12 @@ def algorithm_rho_pollard():
     a = int(input("Enter number a: "))
     b = int(input("Enter number b: "))
     p = int(input("Enter number p: "))
+    divs = algorithm_all_divisors(p - 1)
+    r = p - 1
+    for div in divs:
+        if algorithm_fast_pow(a, div, p) == 1:
+            r = div
+            break
     c = [u, v]
     d = c
     for _ in range(1000):
@@ -20,7 +26,7 @@ def algorithm_rho_pollard():
         if(c_i == d_i):
             a_d = (c[1] - d[1]) % (p - 1)
             b_d = (d[0] - c[0]) % (p - 1)
-            return algorithm_comprasion(a_d, b_d, p - 1)
+            return algorithm_comprasion(a_d, b_d, r)
 
 
 
@@ -30,6 +36,27 @@ def spfunc(a, b, c, p):
         return (c[0] + 1) % (p - 1), c[1] % (p - 1)
     else:
         return c[0] % (p - 1), (c[1] + 1) % (p - 1)
+    
+def algorithm_all_divisors(N):
+    prime_factors = algorithm_rho_pollard_fact(N)
+    if not prime_factors:
+        return [1]  
+
+    unique_factors = list(set(prime_factors))
+    divisors = [1]
+
+    for factor in unique_factors:
+        max_power = prime_factors.count(factor)
+        current_divisors = divisors.copy()
+
+        for power in range(1, max_power + 1):
+            for d in current_divisors:
+                new_divisor = d * (factor ** power)
+                if new_divisor not in divisors:
+                    divisors.append(new_divisor)
+
+    divisors.sort()
+    return divisors
 
 def main():
     print(algorithm_rho_pollard())
